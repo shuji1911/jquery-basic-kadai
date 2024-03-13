bf37a88a0bab84cfd5fdd47d4fb1f78960d5930f
shuji1911 <158697245+shuji1911@users.noreply.github.com>
2024-03-13 10:13:51 +0900

    Update event.js

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,9 +1,8 @@
-
 $(window).on('load',function(){
     console.log('loadイベントが発生しました');
-   });
-   
-   $('body').on('scroll',function(){
-    console.log('scrollイベントが発生しました');
+   });   
+
+   $(window).on('scroll',function(){
+        console.log('scrollイベントが発生しました');   
 });
 
