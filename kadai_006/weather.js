ef32ddadd38ef8301a8ac0dd3c70685e264f377d
shuji1911 <158697245+shuji1911@users.noreply.github.com>
2024-03-13 10:43:47 +0900

    Update weather.js

--- kadai_006/weather.js
+++ kadai_006/weather.js
@@ -1,9 +1,8 @@
 $(function() {
-     // id属性要素がクリックされたら
-      $('#target').on('load', function(){
+     
        // id属性がtargetの要素にclass属性'h1'を追加
-        $('#target').addClass('h1');
+        $('#target').addClass('heading');
       });
-    });
+    
     
     
